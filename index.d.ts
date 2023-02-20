27cde0c058d491b751d5682a4e07a0be6e0b203a
Jonathan <jonathan@stamp.yt>
2023-02-20 11:27:47 +0100

    Add typing

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Say Hello to someone
+ *
+ * @param {string} name The name of someone
+ * @returns {string} The resulting says hello to someone
+ */
+export declare function greeting(name: string): string;
